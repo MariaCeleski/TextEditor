dae6db308f8be48f2a3b5a27e6ef39dc7a94bd77
Maria de Lourdes Celeski <marialceleski@gmail.com>
2022-02-17 07:59:07 -0300

    Iniciando projeto Editor de Texto

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace TextEditor
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+
+        static void Menu()
+        {
+            Console.Clear();
+            Console.WriteLine("O que você deseja fazer?");
+            Console.WriteLine("1 - Abrir arquivo");
+            Console.WriteLine("2 - Criar novo arquivo");
+            Console.WriteLine("0 - Sair");
+            Console.WriteLine("");
+            short option = short.Parse(Console.ReadLine());
+        }
+    }
+}
