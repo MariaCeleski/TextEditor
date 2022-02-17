a7bcc56730eeccd80b4f52329a93b7d2bb8fc969
Maria de Lourdes Celeski <marialceleski@gmail.com>
2022-02-17 11:31:04 -0300

    Criando método abrir

--- Program.cs
+++ Program.cs
@@ -30,10 +30,25 @@ namespace TextEditor
 
             }
 
-
         }
 
-        static void Abrir() { }
+        static void Abrir()
+        {
+            Console.Clear();
+            Console.WriteLine("Qual caminho do arquivo?");
+            var path = Console.ReadLine();
+
+            using (var file = new StreamReader(path))
+            {
+                string text = file.ReadToEnd();
+                Console.WriteLine(text);
+            }
+
+            Console.WriteLine("");
+            Console.ReadLine();
+            Menu();
+
+        }
 
         static void Editar()
         {
